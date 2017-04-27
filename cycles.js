a9ade83012eb9e08845c6fb8a77e87797208f40d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-04-27 17:04:35 -0400

    add title

--- cycles.js
+++ cycles.js
@@ -1,20 +1,30 @@
 let exampleCount = 0
+const examples = document.getElementById('examples')
 
 function addExample (name, initialSource) {
   exampleCount += 1
 
-  let sourceElement = document.createElement('div')
+  const exampleContainer = document.createElement('div')
+  exampleContainer.classList.add('example')
+  examples.appendChild(exampleContainer)
+
+  const titleElement = document.createElement('div')
+  titleElement.classList.add('title')
+  titleElement.innerText = name
+
+  exampleContainer.appendChild(titleElement)
+
+  const sourceElement = document.createElement('div')
   sourceElement.classList.add('source')
 
-  let editor = ace.edit(sourceElement);
+  const editor = ace.edit(sourceElement);
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/javascript");
-  const examples = document.getElementById('examples')
-  examples.appendChild(sourceElement)
+  exampleContainer.appendChild(sourceElement)
 
-  let appElement = document.createElement('div')
+  const appElement = document.createElement('div')
   appElement.setAttribute('id', 'app1')
-  examples.appendChild(appElement)
+  exampleContainer.appendChild(appElement)
 
   editor.getSession().on('change', function(e) {
     const source = packs.stripIndent(editor.getValue())
