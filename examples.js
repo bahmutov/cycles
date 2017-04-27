3b231e1b2282038e3b40314f71565759d0b68805
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-04-27 16:05:48 -0400

    examples in its own file

---
+++ examples.js
@@ -0,0 +1,22 @@
+const inputExample = `
+const {run: Run, dom: DOM } = window.packs
+const {run} = Run
+const {div, label, input, hr, h1, makeDOMDriver} = DOM
+function main({DOM}) {
+  const input$ = DOM.select('.field').events('input')
+  const name$ = input$.map(ev => ev.target.value).startWith('')
+  const vdom$ = name$.map(name =>
+    div([
+      label('Name:'),
+      input('.field', {attrs: {
+        type: 'text', placeholder: 'Type your name'}
+      }),
+      h1('Hello ' + (name || 'stranger')+ '!'),
+    ])
+  )
+
+  return { DOM: vdom$ }
+}
+
+run(main, { DOM: makeDOMDriver('#app1') })
+`
